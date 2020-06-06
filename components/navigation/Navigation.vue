21ec7302b7ef0b596ac6aef87e35008cc032c7ad
Marcus Geil <weblrmail@gmail.com>
2020-06-06 21:32:48 +0200

    Make discord menu item to a link

--- components/navigation/Navigation.vue
+++ components/navigation/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="m-4 mt-0 py-2 border-b-2 border-gray-800 flex items-center">
     <Logo />
     <Menu class="ml-auto">
-      <MenuItem>Discord</MenuItem>
+      <MenuItem><a href="http://gw2.link/redichediscord" target="_blank">Discord</a></MenuItem>
     </Menu>
   </nav>
 </template>
