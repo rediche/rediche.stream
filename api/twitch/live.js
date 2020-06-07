96e828310118709b5fb7b8a47be43c02707cc780
Marcus Geil <weblrmail@gmail.com>
2020-06-07 16:44:29 +0200

    Change live endpoint to not use async function

--- api/twitch/live.js
+++ api/twitch/live.js
@@ -3,14 +3,17 @@ import getStreams from '../../twitch/helix/streams'
 
 const twitchUsername = process.env.TWITCH_USERNAME
 
-module.exports = async (req, res) => {
-  const twitchAuth = await auth()
-  const streams = await getStreams(twitchAuth.access_token, { user_login: twitchUsername})
-
-  const channel = streams.data ? streams.data.find(stream => stream.user_name.toLowerCase() === twitchUsername.toLowerCase()) : false
-
-  res.send({ 
-    live: !!streams.data.length && !!channel,
-    data: channel ? channel : {}
-  })
+module.exports = (req, res) => {
+  auth()
+    .then(creds => getStreams(creds.access_token, { user_login: twitchUsername }))
+    .then(streams => streams.data.length 
+      ? streams.data.find(stream => 
+          stream.user_name.toLowerCase() === twitchUsername.toLowerCase()) 
+      : false
+    )
+    .then(channel => res.send({
+      live: !!channel,
+      data: channel ? channel : {}
+    }))
+    .catch(error => res.send(error))
 }
