5d94a82f1fbafb0c5fe823a37ead2c1b87fe4f41
Marcus Geil <weblrmail@gmail.com>
2020-06-07 16:32:46 +0200

    Check if streams data is available

--- api/twitch/live.js
+++ api/twitch/live.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   const twitchAuth = await auth()
   const streams = await getStreams(twitchAuth.access_token, { user_login: twitchUsername})
 
-  const channel = streams.data.find(stream => stream.user_name.toLowerCase() === twitchUsername.toLowerCase())
+  const channel = streams.data ? streams.data.find(stream => stream.user_name.toLowerCase() === twitchUsername.toLowerCase()) : false
 
   res.send({ 
     live: !!streams.data.length && !!channel,
