0b08cf75e87b1e07d9aea800a454e99a2a6de8b6
Marcus Geil <weblrmail@gmail.com>
2020-07-01 22:08:51 +0200

    Add link to articles excerpt

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -3,7 +3,9 @@
     <h1 class="text-5xl font-black">Articles & guides</h1>
     <ul class="divide-y-2 divide-gray-800">
       <li class="py-6" v-for="(page, index) in pages" :key="index">
-        <h2 class="text-2xl font-black mb-2">{{ page.title }}</h2>
+          <h2 class="text-2xl font-black mb-2">
+            <nuxt-link :to="linkTo(page.slug)">{{ page.title }}</nuxt-link>
+          </h2>
         <div class="font-serif" v-if="page.excerpt">{{ page.excerpt }}</div>
       </li>
     </ul>
@@ -19,6 +21,21 @@ export default {
       pages
     }
   },
+  methods: {
+    linkTo(slug) {
+      return {
+        name: 'articles-slug',
+        params: {
+          slug: slug
+        }
+      }
+    }
+  },
+  head() {
+    return {
+      title: 'Articles & guides'
+    }
+  }
 }
 </script>
 
