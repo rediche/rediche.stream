934a328f3b492dd8cf2da57008dfb6abe3824c5d
Marcus Geil <weblrmail@gmail.com>
2020-07-01 22:13:24 +0200

    Add link to logo

--- components/navigation/Navigation.vue
+++ components/navigation/Navigation.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="m-4 mt-0 py-2 border-b-2 border-gray-800 flex items-center">
-    <Logo />
+    <nuxt-link :to="{ name: 'index' }">
+      <Logo />
+    </nuxt-link>
     <Menu class="ml-auto">
       <MenuItem><nuxt-link :to="{ name: 'articles' }">Articles & guides</nuxt-link></MenuItem>
       <MenuItem><a href="http://gw2.ninja" target="_blank">GW2 Ninja</a></MenuItem>
