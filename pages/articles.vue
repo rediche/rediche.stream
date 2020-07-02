614eaa3620d78ef41a27664cb2bd5f248407a4ef
Marcus Geil <weblrmail@gmail.com>
2020-07-02 20:31:37 +0200

    Sort articles by newest on top

--- pages/articles.vue
+++ pages/articles.vue
@@ -3,15 +3,3 @@
     <nuxt-child />
   </div>
 </template>
-
-<script>
-export default {
-  async asyncData({ $content, params }) {
-    const pages = await $content('articles').fetch()
-
-    return {
-      pages
-    }
-  },
-}
-</script>
