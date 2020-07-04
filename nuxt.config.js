77bacf7b05c26764f5b65e8ea3d88422661d394f
Marcus Geil <weblrmail@gmail.com>
2020-07-04 10:41:08 +0200

    Remove dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,8 +46,6 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
     '@nuxt/content',
   ],
   /*
